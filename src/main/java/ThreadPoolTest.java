64bdae332e98327bfcfb9bb4c13fa445fa62c229
Mero <1043526080@qq.com>
2021-05-29 22:38:01 +0800

    测试代码

--- src/main/java/ThreadPoolTest.java
+++ src/main/java/ThreadPoolTest.java
@@ -1,5 +1,13 @@
+
+import java.util.concurrent.*;
+
 public class ThreadPoolTest {
     public static void main(String[] args) {
-
+        ExecutorService executor = new ThreadPoolExecutor(3, 5, 1L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(3), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        for (int i = 0; i < 8; i++) {
+            executor.execute(()->{
+                System.out.println(Thread.currentThread().getName() + "==>办理业务");
+            });
+        }
     }
 }
